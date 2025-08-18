47a7aa14ad856d50204f0ddb5d3f8e90f8ebebbd
ほし <43630612+hoshi2710@users.noreply.github.com>
2025-08-19 03:44:17 +0900

    CORS 설정 완료

--- src/config/express.js
+++ src/config/express.js
@@ -4,8 +4,17 @@ import { commonResponse } from "../utils/response.js";
 import cookieParser from "cookie-parser";
 export const setupExpress = () => {
   const app = express();
+  const corsOptions = {
+    origin: [
+      process.env.LOCAL_ORIGIN,
+      process.env.PROD_ORIGIN,
+      process.env.FRONT_LOCAL_ORIGIN,
+      process.env.FRONT_DOMAIN,
+    ],
+    credentials: true,
+  };
   app.set("trust proxy", 1);
-  app.use(cors()); // cors 방식 허용
+  app.use(cors(corsOptions)); // cors 방식 허용
   app.use(express.static("public")); // 정적 파일 접근
   app.use(express.json()); // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
   app.use(express.urlencoded({ extended: false })); // 단순 객체 문자열 형태로 본문 데이터 해석
